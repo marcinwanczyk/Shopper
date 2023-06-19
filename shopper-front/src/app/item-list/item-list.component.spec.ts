7f4f72acca0ace301729ed68f00b4e42d9592a72
veeexy <marasek515@gmail.com>
2023-06-19 14:10:13 +0200

    added testing

--- shopper-front/src/app/item-list/item-list.component.spec.ts
+++ shopper-front/src/app/item-list/item-list.component.spec.ts
@@ -1,5 +1,5 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-
+import {FormsModule} from '@angular/forms';
 import {ItemListComponent} from './item-list.component';
 
 describe('ItemListComponent', () => {
@@ -8,7 +8,8 @@ describe('ItemListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ItemListComponent]
+      declarations: [ItemListComponent],
+      imports: [FormsModule]
     })
       .compileComponents();
   }));
@@ -21,5 +22,49 @@ describe('ItemListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add a new item to the list', () => {
+    const initialItemCount = component.items.length;
+    const newItemName = 'orzechy';
+    const newItemAmount = '5';
+
+    component.newItemName = newItemName;
+    component.newItemAmount = newItemAmount;
+    component.addItem();
+
+    expect(component.items.length).toBe(initialItemCount + 1);
+    expect(component.items[initialItemCount].name).toBe(newItemName);
+    expect(component.items[initialItemCount].amount).toBe(parseInt(newItemAmount));
+  });
+
+  it('should not add an item with an invalid name or amount', () => {
+    const initialItemCount = component.items.length;
+    const invalidItemName = '';
+    const invalidItemAmount = '-5';
+
+    component.newItemName = invalidItemName;
+    component.newItemAmount = invalidItemAmount;
+    component.addItem();
+
+    expect(component.items.length).toBe(initialItemCount);
+  });
+
+  it('should edit an existing item in the list', () => {
+    const initialItemCount = component.items.length;
+    const existingItemIndex = 0;
+    const updatedItemName = 'Updated Item';
+    const updatedItemAmount = '10';
+
+    component.items = [
+      {name: 'cebula', amount: 5},
+      {name: 'ziemniak', amount: 10},
+      {name: 'marchewka', amount: 15}
+    ]
+
+    component.editItem({name: updatedItemName, amount: parseInt(updatedItemAmount)});
+    expect(component.items.length).toBe(initialItemCount);
+    expect(component.items[existingItemIndex].name).toBe(updatedItemName);
+    expect(component.items[existingItemIndex].amount).toBe(parseInt(updatedItemAmount));
   });
 });
